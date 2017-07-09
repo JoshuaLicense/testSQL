b16033303cb4045f8d6acdde5743294cb7787c4a
PerfecTiion <josh2351994@hotmail.co.uk>
2017-07-09 22:08:36 +0100

    Recoded features from the master branch
    Added functionality to import a database, and restore a database.
    
    The database scheme (and the amount of rows in each table) will now
    display in the sidebar, and automatically update when any changes are
    made.
    
    Improved the execution process, now displays the result of the query,
    changes are only saved if an actual modification was made to the
    database.
    
    Improved modularity of the application, questions will be an independant
    addition.
    
    Added a favicon and a generic title

--- js/main.js
+++ js/main.js
@@ -8,60 +8,81 @@
 class testSQL {
   constructor(options = {}) {
     this.db = new SQL.Database(options.buffer);
-    this.q = new Question(this.db);
-    this.execute('CREATE TABLE IF NOT EXISTS `ts-questions` (`ts-number` INTEGER PRIMARY KEY, `ts-question` TEXT, `ts-answer` TEXT, `ts-keywords` TEXT, `ts-completed` NULL DEFAULT NULL)');
     console.log('object constructor set');
   }
 
-  static load() {
+  static load(fileName) {
     return new Promise((resolve, reject) => {
       let cachedDatabase = localStorage.getItem('testSQL');
       let sessionKey = window.sessionStorage.getItem('sessionKey');
 
       // Populate with stored database (cached/file) or use the default one
       if (sessionKey) {
-        this.loadFile(sessionKey, resolve, reject);
+        this.loadSession(sessionKey, resolve, reject);
         console.log('From file');
       } else if (cachedDatabase) {
         this.loadCached(cachedDatabase, resolve, reject);
         console.log('Cached Loaded');
+      } else if (fileName) {
+        this.loadFile(fileName, resolve, reject);
       } else {
         testSQL.loadDefault(resolve, reject);
       }
     });
   }
 
-  // TODO Make php return file name of personalized database
-  // TODO Make this one http request instead of two (return the buffer straight from PHP)
-  static loadFile(sessionKey, resolve, reject) {
+  static loadSession(sessionKey, resolve, reject) {
     $.ajax({
       method: 'post',
       url: 'request.php?action=load',
       data: { key: sessionKey },
       dataType: 'json',
       success: function(data) {
-        let xhr = new XMLHttpRequest();
-
-        // TODO Load default database if can't find session (send back default string)
-        // TODO fetch the filename from PHP!
-        xhr.open('GET', data.relativePath, true);
-        xhr.responseType = 'arraybuffer';
-        xhr.onload = function(e) {
-          if (xhr.status === 200) {
-            console.log('Loaded from default.');
-            resolve(new Uint8Array(this.response));
-          } else {
-            reject(Error('Bad, Bad'));
-          }
-        };
-
-        xhr.send();
+        this.loadFile(data.relativePath, resolve, reject);
       },
 
       error: function(data) {
         this.loadDefault();
       },
     });
+  }
+
+  // TODO Make php return file name of personalized database
+  // TODO Make this one http request instead of two (return the buffer straight from PHP)
+  static loadFile(fileName, resolve, reject) {
+    let xhr = new XMLHttpRequest();
+
+    // TODO Load default database if can't find session (send back default string)
+    // TODO fetch the filename from PHP!
+    xhr.open('GET', fileName, true);
+    xhr.responseType = 'arraybuffer';
+    xhr.onload = function(e) {
+      if (xhr.status === 200) {
+        console.log('Loaded from default.');
+        resolve(new Uint8Array(this.response));
+      } else {
+        reject(Error('Bad, Bad'));
+      }
+    };
+
+    xhr.send();
+  }
+
+  importFile(file) {
+    let fileReader = new FileReader();
+    fileReader.onload = () => {
+      clearAll();
+
+      let uInt8 = new Uint8Array(fileReader.result);
+
+      ts = new testSQL({ buffer: uInt8 });
+      ts.save();
+
+      ts.displaySchema();
+
+      // TODO: Questions
+    }
+    fileReader.readAsArrayBuffer(file);
   }
 
   static loadCached(cachedDatabase, resolve, reject) {
@@ -77,19 +98,7 @@ class testSQL {
   }
 
   static loadDefault(resolve, reject) {
-    let xhr = new XMLHttpRequest();
-    xhr.open('GET', 'Chinook_Sqlite.db', true);
-    xhr.responseType = 'arraybuffer';
-    xhr.onload = function() {
-      if (xhr.status === 200) {
-        console.log('Loaded from default.');
-        resolve(new Uint8Array(this.response));
-      } else {
-        reject(Error('Bad, Bad'));
-      }
-    };
-
-    xhr.send();
+    this.loadFile(`Chinook_Sqlite.db`);
   }
 
   save() {
@@ -105,138 +114,158 @@ class testSQL {
     console.log('saved to cache');
   }
 
-  execute(sql, hideErrors) {
+  executeInput(sql, save) {
     try {
-      return this.db.exec(sql);
+      showOutput(this.db.exec(sql));
+
+      let rowsModified = this.db.getRowsModified();
+      // denotes a DELETE, UPDATE, INSERT operation
+      if(rowsModified > 0) {
+        this.displaySchema();
+      }
+
+      // only save the database if rows have been modified or questions.js indicates to
+      if(rowsModified > 0 || save) {
+        this.save();
+      }
+
     } catch (e) {
       console.log(e);
     }
   }
 
-  validateAnswer(sql) {
-    // Remove old alerts
-    cleanView();
+  displaySchema() {
+    // TODO: Could cache this?
+    let schema = this.db.exec(`SELECT "tbl_name" FROM "sqlite_master" WHERE "type" = 'table' AND "tbl_name" != "ts-questions"`);
+    let html = ``;
 
-    // Get the question, based on the current question (COOKIE)
-    let questionNumber = Cookies.get('CurrentQuestion');
-    let questionInfo = this.Questions().get(questionNumber);
-    console.log(questionInfo);
-    if (questionInfo) {
-      /*
-       * Step 1:  Constraint checking
-       */
-      let inputSQL = sql.toUpperCase();
-      let modelSQL = questionInfo[0].values[0][2];
-      let modelConstraints = questionInfo[0].values[0][3];
-      if (modelConstraints) {
-        let constraintsArray = modelConstraints.split(',');
-        let violations = constraintsArray.filter(el => inputSQL.indexOf(el) === -1);
+    schema[0].values.forEach((el) => {
+      let count = this.db.exec(`SELECT COUNT(*) FROM ${el}`);
 
-        // Check if any constraints were violated
-        if (violations.length > 0) {
-          return showResponse(`Looking for the incursion of the keyword: ${violations[0]} but not found`);
-        }
-      }
+      html = html + `<a class="px-3 py-1 list-group-item list-group-item-action justify-content-between" data-name="${el}">${el} <span class="badge badge-default badge-pill">${count[0].values[0]}</span></a>`
+    });
 
-      let response;
-      // What type of statement is the question expecting
-      if (questionInfo[0].values[0][5] == 'INSERT') {
-        response = this.validateInsertAnswer(questionInfo, inputSQL);
-      } else {
-        response = this.validateSelectAnswer(questionInfo, inputSQL);
-      }
-
-      if(response === true) {
-        let isCompleted = questionInfo[0].values[0][4];
-        if (!isCompleted) {
-          this.Questions().markCompleted(questionNumber);
-          this.save();
-          console.log('MARKED');
-        }
-
-        return showResponse(`Well done! The question was solved`, `success`);
-      }
-
-      return response;
-    }
-    return showResponse(`An error occurred while fetching the question`);
-  }
-
-  validateSelectAnswer(questionInfo, inputSQL) {
-    let modelSQL = questionInfo[0].values[0][2];
-    /*
-     * Construct object of objects of each statement
-     */
-    const modelStmt = this.db.prepare(modelSQL);
-    const modelResultObj = [];
-    while (modelStmt.step()) modelResultObj.push(modelStmt.getAsObject());
-    modelStmt.free();
-
-    const inputStmt = this.db.prepare(inputSQL);
-    const inputResultObj = [];
-    while (inputStmt.step()) inputResultObj.push(inputStmt.getAsObject());
-    inputStmt.free();
-
-    /*
-     * Step 2:  Preliminary checks
-     *          Checks for simularity regarding size of rows and selected columns
-     */
-    // TODO input can return no results and will pass the checks
-    if (modelResultObj.length !== inputResultObj.length || !inputResultObj.length) {
-      return showResponse(`Expected a total of ${modelResultObj.length} row(s), instead recieved ${inputResultObj.length}`);
-    }
-
-    // Get the columns
-    const inputCols = Object.keys(inputResultObj[0]);
-    const modelCols = Object.keys(modelResultObj[0]);
-
-    if (inputCols.length !== modelCols.length) {
-      return showResponse(`Expected only the following column(s) to be selected: ${modelCols.join(', ')}`);
-    }
-
-    /*
-     * Step 3:  Check each row individually for simularity
-     */
-    function normalize(colName) {
-      return colName.replace(/[^A-Z()*]+/gi, '');
-    }
-
-    for (let i = 0; i < inputCols.length; i = i + 1) {
-      // Create an object containing the current looped column's rows
-      // .forEach() ~50% speed increase on .map()
-      const modelRowObj = [];
-      modelResultObj.forEach(el => modelRowObj.push(el[normalize(inputCols[i])]));
-
-      const inputRowObj = [];
-      inputResultObj.forEach(el => inputRowObj.push(el[normalize(inputCols[i])]));
-
-      if (modelRowObj.length !== inputRowObj.length) {
-        return showResponse(`Expected only the following columns to be selected: ${modelCols.join(', ')}`);
-      }
-
-      let lastRow;
-      const isEqual = modelRowObj.every((item, idx) => {
-        lastRow = item;
-        return inputRowObj.indexOf(item) !== -1;
-      });
-
-      if (!isEqual) {
-        return showResponse(`The row containing "${lastRow}" in the column ${inputCols[i]} wasn't found`);
-      }
-    }
-
-    return true;
-  }
-
-  Questions() {
-    return this.q;
+    $(`#ts-schema`).html(html);
   }
 }
 
-function showResponse(msg, alertType) {
-  $('#ts-responses').append('<div class="alert alert-' + (alertType || 'danger') + ' mb-1 alert-dismissible fade show"><button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>' + msg + '</div>');
+/* load (async) the required data before instanticing the class
+ * http://stackoverflow.com/questions/24398699/is-it-bad-practice-to-have-a-constructor-function-return-a-promise
+ */
+let ts;
+
+testSQL.load().then((response) => {
+  // load after the database
+  ts = new testSQL({ buffer: response });
+
+  ts.displaySchema();
+},
+
+function (error) {
+  console.log(error);
+});
+
+const showOutput = function showOutput(result) {
+  console.log(result);
+  let html;
+
+  if(result.length > 0) {
+    html = `<div class="mb-2">Number of returned records: ${result[0].values.length}</div>`;
+    html = html + `<div class="table-responsive">`;
+    html = html + `<table class="table table-striped table-sm table-bordered">`;
+
+    result[0].columns.forEach((el) => html = html + `<th>${el}</th>`);
+
+    result[0].values.forEach((el) => {
+      html = html + `<tr>`;
+      el.forEach((el) => html = html + `<td>${el}</td>`);
+      html = html + `</tr>`;
+    });
+    html = html + `</div>`;
+    html = html + `</table>`;
+  } else {
+      showResponse(`${ts.db.getRowsModified()} rows affected`, `success`);
+  }
+
+  $('#ts-result').html(html);
 }
 
-function cleanView() {
+const showResponse = function showResponse(msg, alertType) {
+  $('#ts-responses').append(`<div class="alert alert-${alertType || `danger`} mt-1 mb-0 alert-dismissible fade show"><button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>${msg}</div>`);
+}
+
+const clearView = function cleanView(andInput) {
   $('#ts-responses > div').remove();
+  $('#ts-result').html(`<span class="text-muted">Click "Run SQL" to execute the SQL statement above.</span>`);
+
+  if(andInput) {
+    input.setValue('');
+  }
 }
+
+const clearAllLocalStorage = function clearAllLocalStorage() {
+  localStorage.clear();
+  clearCookies();
+}
+
+const clearCookies = function clearCookies() {
+  Object.keys(Cookies.get()).forEach((cookieName) => Cookies.remove(cookieName));
+}
+
+/* jQuery Event Handlers */
+$(document).ready(function() {
+  /* Database schema tables */
+  $(`#ts-schema`).on(`click`, `a`, function() {
+    let tableName = $(this).data(`name`);
+    let sql = `SELECT * FROM \`${tableName}\``;
+
+    input.setValue(sql);
+    showOutput(ts.db.exec(sql));
+  });
+
+  /* "Run SQL" button */
+  $(`#ts-run`).click(() => {
+    let sql = input.getValue();
+    if (sql) {
+      // Remove old alerts
+      clearView();
+      ts.executeInput(sql);
+    } else {
+      showResponse(`Needs something in it`);
+    }
+  });
+
+  /* "Clear" button */
+  $(`#ts-clear`).click(() => {
+    input.setValue(``);
+  });
+
+  /* Restore icon */
+  $(`#ts-restore`).on(`click`, function() {
+    testSQL.load().then((response) => {
+      clearAllLocalStorage();
+
+      // load after the database
+      ts = new testSQL({ buffer: response });
+
+      ts.displaySchema();
+      // TODO: Questions
+    },
+
+    function (error) {
+      console.log(error);
+    });
+  });
+
+  /* Import icon */
+  $(`#ts-import`).on(`change`, function() {
+    let file = $(this).get(0).files[0];
+
+    ts.importFile(file);
+  });
+
+  /* Help icon */
+  $(`#ts-help`).on(`click`, function() {
+    alert(`help`);
+  });
+});
